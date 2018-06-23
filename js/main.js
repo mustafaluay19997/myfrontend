86443cc484f28e371a59288ca1b6133ba0831abd
Mustafa Luay <mustafaluay997@gmail.com>
2018-06-23 14:00:13 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,14 @@
+
+  $("a").on('click', function(event) {
+    
+        if (this.hash !== "") {
+
+          event.preventDefault();
+          var hash = this.hash;
+          $('html, body').animate({
+            scrollTop: $(hash).offset().top
+          }, 2000, function(){
+            window.location.hash = hash;
+          });
+        } 
+      });
